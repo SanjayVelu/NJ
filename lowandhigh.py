e066d330ec861bd29528b0df31929dd8d36627ea
SanjayVelu <51081868+SanjayVelu@users.noreply.github.com>
2019-08-05 17:28:01 +0530

    Create lowandhigh.py

---
+++ lowandhigh.py
@@ -0,0 +1,8 @@
+Numlist = []
+number = int(input())
+for i in range(1, number + 1):
+    value = int(input())
+    Numlist.append(value)
+Numlist.sort()
+print(Numlist[0])
+print(Numlist{number -1])
