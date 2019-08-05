294371707a49a18f58884806858220d36030cc12
SanjayVelu <51081868+SanjayVelu@users.noreply.github.com>
2019-08-05 19:19:42 +0530

    Update binary.py

--- binary.py
+++ binary.py
@@ -1,11 +1,21 @@
-def check(string):
-    p = set(string)
-    s = {'0', '1'}
-    if s == p or p == {'0'} or p == {'1'}:
-        print("yes")
-    else:
-        print("no")
-if__name__ == "__main__":
-   string = input()
-   check(string)
- 
+def check(string) : 
+  
+    p = set(string) 
+  
+     
+    s = {'0', '1'} 
+  
+  
+    if s == p or p == {'0'} or p == {'1'}: 
+        print("Yes") 
+    else : 
+        print("No") 
+  
+  
+
+if __name__ == "__main__" : 
+  
+    string = input()
+  
+  
+    check(string)
